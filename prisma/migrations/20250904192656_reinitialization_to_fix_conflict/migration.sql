78f8822993858ca17930a1324fed01962382f85a
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2025-09-05 03:27:15 +0800

    fix migration conflict

--- prisma/migrations/20250904192656_reinitialization_to_fix_conflict/migration.sql
+++ prisma/migrations/20250904192656_reinitialization_to_fix_conflict/migration.sql
@@ -20,15 +20,27 @@ CREATE TABLE `User` (
     `avatar` VARCHAR(191) NULL,
     `last_login` DATETIME(3) NULL,
     `role` ENUM('admin', 'employee') NOT NULL,
+    `phone_number` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NULL,
 
     UNIQUE INDEX `User_username_key`(`username`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `ValidQRCode` (
-    `id` VARCHAR(191) NOT NULL,
+CREATE TABLE `Notification` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+    `resource_path` VARCHAR(191) NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `message` TEXT NOT NULL,
+    `read` BOOLEAN NOT NULL DEFAULT false,
+    `recipient_id` VARCHAR(191) NOT NULL,
+    `sender_id` VARCHAR(191) NULL,
 
+    INDEX `Notification_recipient_id_idx`(`recipient_id`),
+    INDEX `Notification_sender_id_idx`(`sender_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -36,11 +48,11 @@ CREATE TABLE `ValidQRCode` (
 CREATE TABLE `Attendance` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `clock_in_at` VARCHAR(191) NULL,
-    `clock_out_at` VARCHAR(191) NULL,
+    `clock_in_at` DATETIME(3) NULL,
+    `clock_out_at` DATETIME(3) NULL,
     `user_id` VARCHAR(191) NOT NULL,
     `overtime_hours` DOUBLE NULL,
-    `status` ENUM('ATTEND', 'ABSENT', 'EXCUSED', 'LATE', 'OVERTIME') NOT NULL,
+    `status` ENUM('ATTEND', 'ABSENT', 'EXCUSED') NOT NULL,
 
     UNIQUE INDEX `Attendance_date_user_id_key`(`date`, `user_id`),
     PRIMARY KEY (`id`)
@@ -57,12 +69,19 @@ CREATE TABLE `Permission` (
     `proof` VARCHAR(191) NULL,
     `status` ENUM('PENDING', 'APPROVED', 'REJECTED') NOT NULL DEFAULT 'PENDING',
     `type` ENUM('FULL', 'EARLY_LEAVE', 'LATE') NOT NULL,
+    `rejected_reason` VARCHAR(191) NULL,
     `user_id` VARCHAR(191) NOT NULL,
     `date_start` DATETIME(3) NOT NULL,
     `date_end` DATETIME(3) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Notification` ADD CONSTRAINT `Notification_recipient_id_fkey` FOREIGN KEY (`recipient_id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Notification` ADD CONSTRAINT `Notification_sender_id_fkey` FOREIGN KEY (`sender_id`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Attendance` ADD CONSTRAINT `Attendance_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- test.ts
+++
@@ -1,9 +0,0 @@
-import { join } from "path";
-
-console.log(
-  join(
-    process.cwd(),
-    "public",
-    "/uploads/proof/489fe7f4-914e-44cf-bbc5-9b00a4d6ff5c_2025-09-03T16-06-08-676Z_e2bf01b9-fb37-4a44-93ac-10946ced6200.jpg"
-  )
-);
